abf8f819ede8dddf81b3f0f5153c5402132a2d21
Jon Hamshaw <jonathanhamshaw@gmail.com>
2013-05-12 23:11:05 +0100

    intial creation of a working, deployable solution

---
+++ ClapOnClapOff/Program.cs
@@ -0,0 +1,46 @@
+﻿using Gadgeteer;
+using Microsoft.SPOT;
+
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+using Gadgeteer.Modules.GHIElectronics;
+
+namespace ClapOnClapOff
+{
+    public partial class Program
+    {
+        // This method is run when the mainboard is powered up or reset.   
+        void ProgramStarted()
+        {
+            /*******************************************************************************************
+            Modules added in the Program.gadgeteer designer view are used by typing 
+            their name followed by a period, e.g.  button.  or  camera.
+            
+            Many modules generate useful events. Type +=<tab><tab> to add a handler to an event, e.g.:
+                button.ButtonPressed +=<tab><tab>
+            
+            If you want to do something periodically, use a GT.Timer and handle its Tick event, e.g.:
+                GT.Timer timer = new GT.Timer(1000); // every second (1000ms)
+                timer.Tick +=<tab><tab>
+                timer.Start();
+            *******************************************************************************************/
+
+
+            // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
+            Debug.Print("Program Started");
+
+            button.ButtonPressed += new Button.ButtonEventHandler(button_ButtonPressed);
+            button.ButtonReleased += new Button.ButtonEventHandler(button_ButtonReleased);
+        }
+
+        void button_ButtonPressed(Button sender, Button.ButtonState state)
+        {
+            multicolorLed.BlinkRepeatedly(Color.Blue);
+        }
+
+        void button_ButtonReleased(Button sender, Button.ButtonState state)
+        {
+            multicolorLed.TurnOff();
+        }
+    }
+}
